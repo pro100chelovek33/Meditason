ff9507f1c834978558eb5f0f440c8a8d03135096
pro100chelovek33 <Zz428159@gmail.com>
2022-06-02 15:21:41 +0500

    project

--- app/src/main/java/com/example/meditason/SignInActivity.kt
+++ app/src/main/java/com/example/meditason/SignInActivity.kt
@@ -1,14 +1,36 @@
 package com.example.meditason
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 
 class SignInActivity : AppCompatActivity() {
+    lateinit var email:EditText
+    lateinit var password:EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        email= findViewById(R.id.email)
+        password=findViewById(R.id.password)
+
     }
 
-    fun signin(view: View) {}
-}
+    fun signin(view: View) {
+
+        if(email.text.toString().isNotEmpty() && password.text.toString().isNotEmpty()){
+    }
+        else{
+
+            val alert = AlertDialog.Builder(this)
+                .setTitle("Ошибка")
+                .setMessage("У вас есть незаполненные поля")
+                .setPositiveButton("Ok",null)
+                .create()
+                .show()
+        }
+    }}
+
+
