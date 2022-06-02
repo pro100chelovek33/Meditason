a1bf1def5d856b2b78d8cd8332de625095bc9338
pro100chelovek33 <Zz428159@gmail.com>
2022-06-02 10:00:27 +0500

    project

--- app/src/main/java/com/example/meditason/MainActivity.kt
+++ app/src/main/java/com/example/meditason/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.meditason
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,5 +11,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun login(view: View) {}
+    fun login(view: View) {
+        val intent = Intent (this, SignInActivity::class.java)
+        startActivity(intent)
+    }
 }
---
+++ app/src/main/java/com/example/meditason/SignInActivity.kt
@@ -0,0 +1,14 @@
+package com.example.meditason
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+
+    fun signin(view: View) {}
+}
